fd9dc1ce07900b164f765214856951c99b661ceb
Jared Slawski <jaredslawski@gmail.com>
2026-04-20 16:39:20 -0600

    Endgame fixes

--- Assets/_Scripts/EndGame.cs
+++ Assets/_Scripts/EndGame.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class EndGame : MonoBehaviour
 {
@@ -8,6 +10,15 @@ public class EndGame : MonoBehaviour
 
     [SerializeField]
     private GameObject _cutscenePanel;
+
+    [SerializeField]
+    private VideoPlayer _cutscenePlayer;
+
+    [SerializeField]
+    private string _cutsceneName;
+
+    [SerializeField]
+    private float _cutsceneLength;
 
     private void Awake()
     {
@@ -21,5 +32,27 @@ public class EndGame : MonoBehaviour
     {
         FaceController.instance.CloseEyes();
         this._cutscenePanel.SetActive(true);
+        this.PlayVideo();
+    }
+
+    private void PlayVideo()
+    {
+        this._cutscenePlayer.Stop();
+
+        string filenameWithExtension = this._cutsceneName + ".webm";
+
+        string filePath = System.IO.Path.Combine(Application.streamingAssetsPath, filenameWithExtension);
+        this._cutscenePlayer.url = filePath;
+
+        this._cutscenePlayer.renderMode = VideoRenderMode.RenderTexture;
+        this._cutscenePlayer.targetCameraAlpha = 1.0f;
+        this._cutscenePlayer.Play();
+
+        Invoke("ResetGame", this._cutsceneLength);
+    }
+
+    private void ResetGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
